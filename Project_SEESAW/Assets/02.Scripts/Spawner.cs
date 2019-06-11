b30f82093c99873dfbefcae00f3c117e30530f30
ParkMinuk <51641452+ParkMinuk@users.noreply.github.com>
2019-06-11 16:07:55 +0900

    eye exercise
    Spawner 스크립트 추가
    sss Scene 추가

---
+++ Project_SEESAW/Assets/02.Scripts/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Get_Key : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startwait;
+    public bool stop;
+
+    int randEnemy;
+
+    void Start()
+    {
+        StartCoroutine(waitGet_Key());
+    }
+
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+
+    IEnumerator waitGet_Key()
+    {
+        yield return new WaitForSeconds(startwait);
+
+        while (!stop)
+        {
+            randEnemy = Random.Range(0, 2);
+
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1, Random.Range(-spawnValues.z, spawnValues.z));
+
+            Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
