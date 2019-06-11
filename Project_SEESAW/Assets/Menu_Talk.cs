15f1cec7774415c3b9a0fdbc4fd5d192b95fa361
SmokyOnion <tomekichi1551@gmail.com>
2019-06-11 20:47:04 +0900

    Button, Slider with Leap Motion
    - Added Leap Motion test Scene
    - Added Curved UI

---
+++ Project_SEESAW/Assets/Menu_Talk.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Talk : MonoBehaviour
+{
+    public void ButtonSay(int num)
+    {
+        Debug.Log(num + "Button Clicked!");
+    }
+
+    public void SliderSay(GameObject tmp)
+    {
+        Debug.Log(123);
+    }
+}
