75f4e718e704bbc230506c7f5d45fcd0ad5eff06
SmokyOnion <tomekichi1551@gmail.com>
2019-06-13 22:47:07 +0900

    Create Animation for Menu
    - Added Animator in MainMenu
    - Create Animation (MenuTurnOn, MenuTurnOff)
    - Added some script in MenuSystem

---
+++ Project_SEESAW/Assets/02.Scripts/MenuSystem.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSystem : MonoBehaviour
+{
+    [Header("Objects")]
+    public GameObject menu;
+    public GameObject ui;
+    public GameObject toggle;
+    public GameObject camera;
+
+    [Header("float"), Range(0.2f, 0.8f)]
+    public float distance;
+
+    private Animator menuAni;
+
+    void Start()
+    {
+        menuAni = menu.GetComponent<Animator>();
+
+        ToggleMenu(false);
+        menu.SetActive(false);
+    }
+
+    //UI Toggle 이벤트
+    public void ToggleMenu(bool tog)
+    {
+        if(tog == true)
+        {
+            toggle.SetActive(false);
+            ui.SetActive(true);
+        }
+        else if(tog == false)
+        {
+            toggle.SetActive(true);
+            ui.SetActive(false);
+        }
+    }
+
+    //Menu 활/비활성화
+    public void TurnOnMenu()
+    {
+        menu.transform.position = (camera.transform.forward * distance) + new Vector3(0, camera.transform.position.y, 0);
+        Debug.Log(camera.transform.forward);
+        menu.transform.rotation = Quaternion.Euler(-90, camera.transform.eulerAngles.y, 0);
+        menu.SetActive(true);
+        menuAni.SetBool("isTurnOn", true);
+    }
+
+    public void TurnOffMenu()
+    {
+        menuAni.SetBool("isTurnOn", false);
+        StartCoroutine(WaitForAnimation(menuAni));
+    }
+
+    IEnumerator WaitForAnimation(Animator animator)
+    {
+        while(false == animator.IsInTransition(0))
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        //menu.SetActive(false);
+    }
+}
--- Project_SEESAW/Assets/02.Scripts/Menu_Talk.cs
+++ Project_SEESAW/Assets/02.Scripts/Menu_Talk.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Menu_Talk : MonoBehaviour
 {
+    List<Thread> tmp;
+
+    private void Start()
+    {
+        
+    }
+
     public void ButtonSay(int num)
     {
         Debug.Log(num + "Button Clicked!");
