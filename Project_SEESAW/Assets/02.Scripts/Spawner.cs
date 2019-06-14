81418df534bed217fb04dedfe59fd5377df8086d
ParkMinuk <51641452+ParkMinuk@users.noreply.github.com>
2019-06-14 13:50:36 +0900

    eye Circle
    Random Range
    Instantiate
    IEnumerator
    Destroy

--- Project_SEESAW/Assets/02.Scripts/Spawner.cs
+++ Project_SEESAW/Assets/02.Scripts/Spawner.cs
@@ -2,41 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Get_Key : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
     public GameObject[] enemies;
     public Vector3 spawnValues;
     public float spawnWait;
-    public float spawnMostWait;
-    public float spawnLeastWait;
     public int startwait;
     public bool stop;
 
+    [Header("Spawn Points")]
+    public GameObject LT;
+    public GameObject RB;
+
     int randEnemy;
 
+
+    //
     void Start()
     {
         StartCoroutine(waitGet_Key());
+        spawnWait = 3.0f;
     }
 
+    //
     void Update()
     {
-        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+        //spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+        
     }
 
     IEnumerator waitGet_Key()
     {
         yield return new WaitForSeconds(startwait);
 
+        float left = LT.transform.position.x;
+        float right = RB.transform.position.x;
+        float top = LT.transform.position.z;
+        float bottom = RB.transform.position.z;
+
         while (!stop)
         {
-            randEnemy = Random.Range(0, 2);
+            randEnemy = Random.Range(0, 1);
 
-            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1, Random.Range(-spawnValues.z, spawnValues.z));
+            Vector3 spawnPosition = new Vector3(Random.Range(left, right), 1, Random.Range(top, bottom));
 
-            Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            GameObject tmp = Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
 
             yield return new WaitForSeconds(spawnWait);
+
+            Destroy(tmp);
         }
     }
+
 }
