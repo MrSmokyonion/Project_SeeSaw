fe70ca1b19d45ee9aa94487957afe755d1c84f38
dall4e <dall4e@users.noreply.github.com>
2019-06-17 21:35:22 +0900

    이거슨 음성인식
    음성인식

--- Project_SEESAW/Assets/Scripts/rotateImage.cs
+++ Project_SEESAW/Assets/Scripts/rotateImage.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
+using Microsoft.CognitiveServices.Speech;
+
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class rotateImage : MonoBehaviour
 {
@@ -12,6 +16,14 @@ public class rotateImage : MonoBehaviour
 
     private double eyeSight = 0.1;
     private int wrongAnswer = 0;
+
+    public Text outputText;
+    public Button startRecoButton;
+
+    private object threadLocker = new object();
+    private bool waitingForReco;
+    private string message;
+    private bool micPermissionGranted = false;
 
     public Transform LocalTransform
     {
@@ -26,6 +38,33 @@ public class rotateImage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (outputText == null)
+        {
+            UnityEngine.Debug.LogError("outputText property is null! Assign a UI Text element to it.");
+        }
+        else if (startRecoButton == null)
+        {
+            message = "startRecoButton property is null! Assign a UI Button to it.";
+            UnityEngine.Debug.LogError(message);
+        }
+        else
+        {
+            // Continue with normal initialization, Text and Button objects are present.
+
+#if PLATFORM_ANDROID
+            // Request to use the microphone, cf.
+            // https://docs.unity3d.com/Manual/android-RequestingPermissions.html
+            message = "Waiting for mic permission";
+            if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+            {
+                Permission.RequestUserPermission(Permission.Microphone);
+            }
+#else
+            micPermissionGranted = true;
+            message = "Click button to recognize speech";
+#endif
+            startRecoButton.onClick.AddListener(ButtonClick);
+        }
         RandomRotate();
         m_text.text = eyeSight.ToString();
     }
@@ -33,7 +72,25 @@ public class rotateImage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+#if PLATFORM_ANDROID
+        if (!micPermissionGranted && Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            micPermissionGranted = true;
+            message = "Click button to recognize speech";
+        }
+#endif
+
+        lock (threadLocker)
+        {
+            if (startRecoButton != null)
+            {
+                startRecoButton.interactable = !waitingForReco && micPermissionGranted;
+            }
+            if (outputText != null)
+            {
+                outputText.text = message;
+            }
+        }
     }
 
     private void Check(int direction)
@@ -87,5 +144,51 @@ public class rotateImage : MonoBehaviour
     {
         Check(direction);
         RandomRotate();
+    }
+
+    public async void ButtonClick()
+    {
+        // Creates an instance of a speech config with specified subscription key and service region.
+        // Replace with your own subscription key and service region (e.g., "westus").
+        var config = SpeechConfig.FromSubscription("eeeef68ccdef44588e2c2e896e26fceb", "westus");
+
+        // Make sure to dispose the recognizer after use!
+        using (var recognizer = new SpeechRecognizer(config))
+        {
+            lock (threadLocker)
+            {
+                waitingForReco = true;
+            }
+
+            // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+            // single utterance is determined by listening for silence at the end or until a maximum of 15
+            // seconds of audio is processed.  The task returns the recognition text as result.
+            // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+            // shot recognition like command or query.
+            // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+            var result = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
+
+            // Checks result.
+            string newMessage = string.Empty;
+            if (result.Reason == ResultReason.RecognizedSpeech)
+            {
+                newMessage = result.Text;
+            }
+            else if (result.Reason == ResultReason.NoMatch)
+            {
+                newMessage = "NOMATCH: Speech could not be recognized.";
+            }
+            else if (result.Reason == ResultReason.Canceled)
+            {
+                var cancellation = CancellationDetails.FromResult(result);
+                newMessage = $"CANCELED: Reason={cancellation.Reason} ErrorDetails={cancellation.ErrorDetails}";
+            }
+
+            lock (threadLocker)
+            {
+                message = newMessage;
+                waitingForReco = false;
+            }
+        }
     }
 }
